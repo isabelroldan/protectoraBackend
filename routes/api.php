69cc4a22178c996984c1fa6bf163f969eda66f46
Isabel Roldán Córdoba <isabelroldancordoba@hotmail.com>
2025-06-05 20:27:33 +0200

    IRC Limpiando codigo

--- routes/api.php
+++ routes/api.php
@@ -7,19 +7,6 @@ use App\Http\Controllers\LoginController;
 use App\Http\Middleware\VerificarToken;
 
 
-/* Route::post('login', action: [LoginController::class, 'login']);
-
-Route::middleware(['auth:sanctum'])->group(function () {
-    Route::get('/mascotas/paginadas', [MascotaController::class, 'paginated']);
-    Route::get('/usuarios/paginados', [UsuarioController::class, 'paginated']);
-    Route::get('/solicitudes/paginadas', [SolicitudDeAdopcionController::class, 'paginated']);
-
-    Route::apiResource('mascotas', MascotaController::class);
-    Route::apiResource('usuarios', UsuarioController::class);
-    Route::apiResource('solicitudes', SolicitudDeAdopcionController::class);
-    Route::get('logout', [LoginController::class, 'logout']);
-}); */
-
 Route::post('login', [LoginController::class, 'login']);
 
 // Rutas accesibles solo con autenticación
